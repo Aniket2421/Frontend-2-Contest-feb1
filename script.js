234905a811a562597c7cbe475cdb185b343566a7
Aniket2421 <aniketnerkar123@gmail.com>
2023-02-11 18:15:24 +0530

    commit_msg

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
-/** @format */
-
 let arr = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
+let arr2 = [
+  { id: 1, name: "Virat", age: "18", marks: 90 },
+  { id: 2, name: "David", age: "20", marks: 60 },
+  { id: 3, name: "Smith", age: "19", marks: 45 },
+]
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+  
+  let Passstudent = arr.filter((stu) => stu.marks > 50);
+  console.log(Passstudent);
 }
+PrintStudentswithMap();
 
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
-
-function addData() {
-  //Write your code here, just console.log
-}
+arr.forEach(student => {
+  if (student.marks > 50) {
+    console.log(student);
+  }
+});
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  
+  let failedStudent = arr.filter((stu) => stu.marks >= 50);
+  console.log(failedStudent);
 }
+removeFailedStudent();
+
+function addData() {
+  let newStudent = { id: 4, name: "Susan", age: 20, marks: 45 };
+  arr.push(newStudent);
+  console.log(newStudent);
+}
+addData();
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let combinedArray = arr.concat(arr2);
+  console.log(combinedArray);
 }
+concatenateArray();
+
+
